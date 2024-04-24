cab358a8a795f0b736eb404371201fd85240c27c
Chris Darden <chris.a.darden@gmail.com>
2024-04-23 19:38:10 -0500

    Update form validation and error messages in CardFormComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [AppComponent, CardFormComponent, InputComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {}
--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -9,16 +9,40 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [
-      // Validators.required,
-      // Validators.minLength(3),
-      // Validators.maxLength(5),
-      Validators.pattern(/\s/)
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/^[a-zA-Z\s]*$/)
     ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+      Validators.pattern(/^\d+$/)
+    ]
+    ),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]
+    ),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+      Validators.pattern(/^\d+$/)
+    ]
+    )
   });
 
   constructor() {
     console.log(this.cardForm.get('name'));
   }
 
-  ngOnInit() {}
+  onSubmit() {
+    console.log("form submitted");
+  }
+
+  ngOnInit() {
+  }
 }
